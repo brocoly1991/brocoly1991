770c8f1f3c530653cb3e44f7282f7390e532de00
brocoly91 <pignecro4@naver.com>
2021-07-14 19:45:17 +0900

    commit

--- payload/presentation.ts
+++ payload/presentation.ts
@@ -4,24 +4,24 @@ const presentation: IPresentation.Payload = {
   disable: false,
 
   list: [
-    {
-      title: '카메라파이 스토어',
-      subTitle: '카메라파이 스토어 react',
-      at: '2021-07',
-      descriptions: [
-        {
-          content: '카메라파이 스토어 사이트를 react 리뉴얼',
-        },
-        {
-          content: 'git : ',
-          postHref: 'https://github.com/brocoly1991/storeReact',
-        },
-      ],
-    },
+    // {
+    //   title: '카메라파이 스토어',
+    //   subTitle: '카메라파이 스토어 react',
+    //   at: '2021-07',
+    //   descriptions: [
+    //     {
+    //       content: '카메라파이 스토어 사이트를 react 리뉴얼',
+    //     },
+    //     {
+    //       content: 'git : ',
+    //       postHref: 'https://github.com/brocoly1991/storeReact',
+    //     },
+    //   ],
+    // },
     {
       title: 'YogiyoToyProjectReact',
       subTitle: 'YogiyoToyProjectReact',
-      at: '2021-05',
+      at: '2021-07',
       descriptions: [
         {
           content: '요기요 사이트를 react로 구현',
